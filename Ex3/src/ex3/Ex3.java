22fe1218bd9d625ab9a01b6445e88801d460c3d9
unifrsilva <41764187+unifrsilva@users.noreply.github.com>
2019-02-16 12:32:51 -0200

    Exercicio 3

---
+++ Ex3/src/ex3/Ex3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex3;
+import java.util.Scanner;
+
+/**
+ *
+ * @author unifrsilva
+ */
+//Exercicio 3 - Números Ímpares
+public class Ex3 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i = 1;
+       while(i<=1000)
+       {
+           System.out.printf("%d\n",i);
+           i = i + 2;
+       }
+        // TODO code application logic here
+    }
+}
