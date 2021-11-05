2c2f04505cd56d6fe4a8313b47c850c95b9a4eb7
Renan Martins <renanmartins1999@hotmail.com>
2021-11-05 16:06:04 -0300

    Finalizando SwapperDemo

--- Aula 2/SwapperDemo.java
+++ Aula 2/SwapperDemo.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
+class Swapper {
+  private float x, y;
+  
+  public float getX(){
+    return x;
+  }
+
+  public float getY(){
+    return y;
+  }
+
+  public void setX(float x){
+    this.x = x;
+  }
+
+  public void setY(float y){
+    this.y = y;
+  }
+
+  public void swap(float x, float y){
+    float temp = x;
+    this.x = y;
+    this.y = temp;
+  }
+}
+
 public class SwapperDemo {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      Swapper troca = new Swapper();
+      Scanner input = new Scanner(System.in);
+
+
+      troca.setX(input.nextFloat()); // Setando valor de x
+      troca.setY(input.nextFloat()); // Setando valor de y
+
+      System.out.println("x = " + troca.getX());
+      System.out.println("y = " + troca.getY());
       
+      // Trocando valores de x e y
+      troca.swap(troca.getX(), troca.getY());
+
+      System.out.println("x = " + troca.getX());
+      System.out.println("y = " + troca.getY());
+      
+
       // TODO code application logic here
     }
 
